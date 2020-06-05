c353fc656d45bac3d8bf72112bf27fb21a0f1b01
xxdavis97 <xxdavis97@gmail.com>
2020-06-05 10:01:04 -0400

    outside

--- application.py
+++ application.py
@@ -1,4 +1,6 @@
-import app
+import app as application
+
+app = application.app
 
 if __name__ == "__main__":
-    app.app.run_server(debug=True, host="0.0.0.0")
+    app.run_server(debug=True, host="0.0.0.0")
