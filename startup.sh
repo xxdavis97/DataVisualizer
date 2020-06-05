9f129028b173ab20b2a24711c4d8c9ef57386f54
xxdavis97 <xxdavis97@gmail.com>
2020-06-05 11:06:11 -0400

    g

--- startup.sh
+++ startup.sh
@@ -5,4 +5,4 @@ pip3 install Flask==1.1.2
 pip3 install numpy==1.18.0
 pip3 install pandas==1.0.4
 pip3 install requests==2.12.4
-gunicorn --bind=0.0.0.0 --timeout 600 application:app
+python application.py
