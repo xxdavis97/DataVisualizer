ec0d2550993dd4cd9ff4b1e14512abde424ad590
xxdavis97 <xxdavis97@gmail.com>
2020-06-08 12:10:38 -0400

    about page

---
+++ AboutContent.py
@@ -0,0 +1,99 @@
+import dash_core_components as dcc
+import dash_html_components as html
+
+
+ABOUT_CONTENT = html.Div(children= [
+    html.Div(children = [
+        html.Div(id="selfie", className="six columns", children = [
+            html.Img(src="/assets/me.png", className="myBioPic"),
+            html.Div(className="about", children=[
+                html.H2("About"),
+                html.P("Jared graduated from Stevens Institute Of Technology in 2020 with a Bachelor of Science for Computer Science in"
+                       "addition to double minoring in Quantitative Finance as well as Pure & Applied Mathematics.  After graduation "
+                       "Jared started his career at Goldman Sachs where he is a Software Engineer for the Consumer and Investment "
+                       "Management Division.  Outside of work, Jared enjoys to work on coding side projects such as this website.  Some "
+                       "of Jared's non-coding related interests include travelling, skiing, golfing and reading.  ")
+            ])
+        ]),
+        html.Div(id="skills", className="six columns", children = [
+            html.Div(className="skills", children=[
+                html.H2("Core Skills And Abilities"),
+                html.Ul(children=[
+                    html.Li(
+                        "Programming experience in Python, Java, JavaScript, SQL, C#, Git, Angular, Neo4j, HTML, CSS"),
+                    html.Li(
+                        "Well versed in popular Python libraries such as Numpy, Pandas, Matplotlib, Requests, Beautiful Soup, Pyodbc, "
+                        "Selenium, Xlwings, Flask and Dash (the library used to create this site)"),
+                    html.Li(
+                        "As a side project developed a trend following stock algorithm deployed on a raspberry pi server that sends email "
+                        "alerts of potential stock picks."),
+                    html.Li(
+                        "A second side project was the development of this website, which uses Python, notably Dash, Flask, Numpy, Pandas, "
+                        "Requests, and Beautiful Soup")
+                ])
+            ])
+        ]),
+    ], className="row aboutRow1 aboutRow"),
+    html.Br(),
+    html.Div(className="row aboutRow", children=[
+        html.H2("Work Experience")
+    ]),
+    html.Div(className="row aboutRow workRow", children=[
+        html.Div(className="workInput", children = [
+            html.H6("Goldman Sachs - New York, NY"),
+            html.Ul(children=[
+                html.Li(
+                    "From scratch created an application utilizing Angular, Java, and SQL to allow the business to gain streamlined"
+                    " visibility of critical missing data for our private equity investments, allowing for easy correction."),
+                html.Li(
+                    "Developed a dashboard with regular database interaction to easily see data errors that were otherwise quite"
+                    " difficult to find and led to far more accurate reporting to our clients."),
+                html.Li("Maintained regular communication with the business side, the main stakeholders, "
+                        "to ensure a clean and useful user experience."),
+            ])
+        ]),
+    ]),
+    html.Div(className="row aboutRow workRow", children=[
+        html.Div(className="workInput", children=[
+            html.H6("Virtual Facility - New York, NY"),
+            html.Ul(children=[
+                html.Li("Modified production level web applications using the Angular JavaScript framework, powered and optimized "
+                        "by a Neo4j Graph Databaseto allow for significantly improved performance over large datasets."),
+                html.Li("Reporting to the CEO, headed the company’s switch to the Agile software development process using Atlassian’s JIRA."),
+                html.Li("Created a dashboard for top-level management, as well as developers, to gain insight into "
+                        "the velocity of our team.  This enabled much better project planning and led to a dramatic increase in efficiency."),
+            ])
+        ]),
+    ]),
+    html.Div(className="row aboutRow workRow", children=[
+        html.Div(className="workInput", children=[
+            html.H6("Sphere Technology Solutions - Jersey City, NJ"),
+            html.Ul(children=[
+                html.Li("Played a major role as a full stack developer towards development and maintenance of Sphere’s leading "
+                        "product, SphereBoard."),
+                html.Li("Generated interactive pages through the use of JavaScript/JQuery, HTML, and CSS, in addition to server-side "
+                        "C# and SQL, that allowed clients to gain a more transparent view into the security of their infrastructure."),
+                html.Li("Utilized Agile/Scrum software development framework to complete product releases efficiently within a team"),
+                html.Li("Heavily worked with the model view controller web application design pattern, coupled with the ASP.NET "
+                        "Web API framework, to develop features for high profile clients")
+            ])
+        ]),
+    ]),
+    html.Div(className="row aboutRow workRow", children=[
+        html.Div(className="workInput", children=[
+            html.H6("Brigade Capital Management - New York, NY"),
+            html.Ul(children=[
+                html.Li("Developed scripts in python to manage and analyze large stores of investment data, including devising "
+                        "a class to call any SQL query, procedure, or file directly from Python and store the result in a dataframe.  This "
+                        "enabled Brigade to run Python’s powerful data analysis functions directly on its SQL databases."),
+                html.Li("Created a python class featuring vectorized methods to prioritize execution speed.  "
+                        "All functions were customized to be imported into MS Excel to facilitate comparisons of returns, or any other source of data, either with a normal distribution or with another dataset.  "
+                        "This allowed people without coding know-how to leverage these powerful data analytics tools."),
+                html.Li("Created automatic email alerts in SQL to notify traders of important price and metric movements in addition "
+                        "to alerts showing daily, weekly, and monthly changes in a wide array of portfolio level risk statistics."),
+                html.Li("Generated plugins to make Python analyses user friendly in MS Excel.")
+            ])
+        ]),
+    ]),
+    html.Br()
+])
---
+++ EquityVisualizerContent.py
@@ -0,0 +1,65 @@
+import dash_core_components as dcc
+import dash_html_components as html
+
+
+EQUITY_VISUALIZER_CONTENT = html.Div(children= [
+    html.Div(
+        className="headerBox",
+        children=[
+            html.Div(
+                className = "searchParameterContainer",
+                children= [
+                    html.Div(children="Input ticker: "),
+                    # Change default back to SPY
+                    dcc.Input(id="Symbolinput",value="SNAP",type="text"),
+                    # html.Button(id="newStock"),
+                    dcc.Input(id="StartInput",value="",type="date"),
+                    dcc.Input(id="EndInput",value="",type="date"),
+                    dcc.RadioItems(
+                        className="candleToggle",
+                        options = [
+                            {'label': 'Price Plot', 'value': False},
+                            {'label': 'Candlestick Plot', 'value': True}],
+                        id = "candleToggle",
+                        value= False
+                    ),
+                    dcc.Dropdown(
+                        className= "optionDrop",
+                        options = [
+                            # {'label': 'Price', 'value': 'price'},
+                            {'label': 'Bollinger Bands', 'value': 'check'},
+                            # {'label': 'Candlestick', 'value': 'candle'},
+                            {'label': '200 Day SMA', 'value': '200SMA'},
+                            {'label': '150 Day SMA', 'value': '150SMA'},
+                            {'label': '100 Day SMA', 'value': '100SMA'},
+                            {'label': '50 Day SMA', 'value': '50SMA'},
+                            {'label': '50 Day EWMA', 'value': '50EWMA'},
+                            {'label': '20 Day EWMA', 'value': '20EWMA'},
+                            {'label': '14 Day RSI (SMA)', 'value': '14SRSI'},
+                            {'label': '14 Day RSI (EWMA)', 'value': '14ERSI'},
+                        ],
+                        id = 'bollinger',
+                        # value = ["price"],
+                        multi=True,
+                        searchable=False,
+                        placeholder="More options"
+                    )
+            ])
+    ]),
+    html.Div(id="output-graph"),
+    html.Div(id="twoGraphCol", children = [
+        html.Div(id="company-bio", className="six columns"),
+        html.Div(id="earnings-graph", className="six columns"),
+
+    ], className="row"),
+    html.Div(id="earningsAndAbout", children = [
+        html.Div(id="ownership-pie", className="six columns"),
+        html.Div(id="shortTable", className="six columns"),
+
+    ], className="row"),
+    html.Div(id="fundOwnership", children=[
+        html.Div(id="top-institutional", className="six columns owner"),
+        html.Div(id="top-mutual", className="six columns owner"),
+
+    ], className="row"),
+])
