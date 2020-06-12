8689bfbf003ea6b76bdcb83930abe3951dbbdd5a
xxdavis97 <xxdavis97@gmail.com>
2020-06-12 10:39:38 -0400

    fix deploy

--- application.py
+++ application.py
@@ -671,6 +671,6 @@ def storeSession(inputData):
 ##################################################################
 if __name__ == '__main__':
     # For deployment
-    # application.run(debug=True, host='0.0.0.0', port='80')
+    application.run(debug=True, host='0.0.0.0', port='80')
     # For local
-    application.run(debug=False)
+    # application.run(debug=False)
