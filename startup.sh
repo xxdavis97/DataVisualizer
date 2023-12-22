e4ea5faa0189474bb1f684a3d651266148a52f0c
xxdavis97 <43418361+xxdavis97@users.noreply.github.com>
2023-12-22 13:21:34 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 pip3 install bs4==0.0.1
 pip3 install dash==2.14.1
-pip3 install dash-table==4.7.0
+pip3 install dash-table==5.0.0
 pip3 install Flask>=2.2.2
 pip3 install numpy==1.18.0
 pip3 install pandas==1.0.4
