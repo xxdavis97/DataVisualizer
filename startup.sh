dbc1141a577139150d72ccc0ee669cea1d0db416
Jared Davis <xxdavis97@gmail.com>
2023-12-23 10:14:04 -0500

    Change startup.sh

--- startup.sh
+++ startup.sh
@@ -5,4 +5,5 @@ pip3 install Flask>=2.2.2
 pip3 install numpy==1.18.0
 pip3 install pandas==1.0.4
 pip3 install requests==2.12.4
-python application.py
+#python application.py
+gunicorn --bind=0.0.0.0 --timeout 600
