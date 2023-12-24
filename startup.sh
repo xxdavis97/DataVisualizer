951995611200aa861da55fdaab03f0ff30049bf2
Jared Davis <xxdavis97@gmail.com>
2023-12-24 10:53:54 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -6,4 +6,4 @@ pip3 install numpy==1.18.0
 pip3 install pandas==1.0.4
 pip3 install requests==2.12.4
 #python application.py
-gunicorn --bind=0.0.0.0 --timeout 600
+gunicorn --bind=0.0.0.0 --timeout 600 application:app
